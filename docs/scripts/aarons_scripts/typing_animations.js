ce47cc406ba333dcf0af213a1e2851ceeb6618cf
aaron-watts <retrolasered@gmail.com>
2021-10-27 11:26:37 +0100

    ~~ aaron.html // change typing animations article

--- docs/scripts/aarons_scripts/typing_animations.js
+++ docs/scripts/aarons_scripts/typing_animations.js
@@ -1,15 +1,28 @@
+const typingAnimationTarget = document.querySelector('#typing-animations .target');
+const liveDemoInteractions = document.querySelectorAll('#typing-animations .live-demo a');
+
 let typeInterval;
 
-const typeText = async (text, elem) => {
+const typeText = (text, elem) => {
+    // console.log(text)
     if (text.length > 0) {
         elem.innerText += text[0];
 
-        if (elem.classList.contains('home-bash')) {             // we only want to cancel out nav commands
-            typeInterval = setTimeout(function () {             // assigned so can be cancelled
-                typeText(text.substring(1, text.length), elem) 
-            }, 60);
-        }
+        typeInterval = setTimeout(function () {
+            typeText(text.substring(1, text.length), elem)
+        }, 60);
     }
 
     else return;
 }
+
+liveDemoInteractions.forEach(anchor => {
+    anchor.addEventListener('mouseover', (evt) => {
+        typeText(evt.target.attributes.command.value, typingAnimationTarget);
+    })
+
+    anchor.addEventListener('mouseout', (evt) => {
+        clearInterval(typeInterval);
+        typingAnimationTarget.innerText = '';
+    })
+})
