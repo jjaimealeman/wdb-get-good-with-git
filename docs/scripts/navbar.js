651b3f067acde14dccafeb567997495f9ecba62c
Shakti <shaktisarkar1999@gmail.com>
2021-11-03 13:29:14 +0530

    was not working on mobile. fixed it!

--- docs/scripts/navbar.js
+++ docs/scripts/navbar.js
@@ -1,14 +1,15 @@
 const navBar = document.querySelector('.main-nav ul');
-const hamBurger = document.querySelector('.hamburger');
-
-hamBurger.addEventListener('click',() => {
-    navBar.classList.toggle('visible')
-    if (hamBurger.getAttribute('src') === 'shakti-images/menu.png') {
-        hamBurger.setAttribute('src', 'shakti-images/close.png');
-        document.body.style.overflow = 'hidden';
-    } else {
-        hamBurger.setAttribute('src', 'shakti-images/menu.png');
-        document.body.style.overflow = 'visible';
-
-    }
-})
+const hamBurger = document.querySelector('.child');
+const parent = document.querySelector(".parent");
+parent.addEventListener("click", function(e) {
+     const child = e.target.matches(".child, .child *");
+     if (child) { navBar.classList.toggle('visible')
+     if (hamBurger.getAttribute('src') === 'shakti-images/menu.png') {
+         hamBurger.setAttribute('src', 'shakti-images/close.png');
+         document.body.style.overflow = 'hidden';
+     } else {
+         hamBurger.setAttribute('src', 'shakti-images/menu.png');
+         document.body.style.overflow = 'visible';
+ 
+     }
+}});
